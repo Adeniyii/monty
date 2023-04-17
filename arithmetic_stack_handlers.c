818ab420376f3445bb7cc558037203467f1d93b7
Adeniyii <ifedayoadeniyi@gmail.com>
2023-04-17 15:11:46 +0100

    fix: update err message

--- arithmetic_stack_handlers.c
+++ arithmetic_stack_handlers.c
@@ -161,7 +161,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (!after_head)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
